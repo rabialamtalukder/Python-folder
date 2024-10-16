8f13337a7e326a67823227f95459921c418981e4
root <40955470+GHasanKaraman@users.noreply.github.com>
2024-10-16 16:32:48 -0400

    added while loop

--- firstpythoncode.py
+++ firstpythoncode.py
@@ -1,22 +1,29 @@
-x = int(input("Enter the first number: "))
-y= int(input("Enter the second number: "))
+while(True):
+    x = int(input("Enter the first number: "))
+    y = int(input("Enter the second number: "))
 
-print("======================")
+    print("======================")
 
-print("1) + Adition")
-print("2) - Sub")
-print("3) * Multiplication")
-print("4) / Division")
+    print("1) + Adition")
+    print("2) - Sub")
+    print("3) * Multiplication")
+    print("4) / Division")
+    print("5) X Exit")
 
 
-print("======================")
-z= int (input("Enter the 1, 2, 3 or 4 : "))
+    print("======================")
+    z = int (input("Enter the 1, 2, 3 or 4 : "))
 
-if (z == 1):
-    print(x + y)
-elif (z == 2):
-    print(x - y)
-elif (z == 4):
-    print(x / y)
-else:
-    print(x*y)
+    if (z == 1):
+        print(x + y)
+    elif (z == 2):
+        print(x - y)
+    elif(z == 3):
+        print(x*y)
+    elif (z == 4):
+        print(x / y)
+    elif (z == 5):
+        break
+    else:
+        print("Please, enter correct function!!!!!")
+
